d02c3ca12d76a2fbee92c9743c5539178699069e
hveldstra <h@veldstra.org>
2017-04-28 06:09:49 +0100

    test: Add basic tests

---
+++ test/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const test = require('tape');
+const EventEmitter = require('events');
+
+const KinesisEngine = require('..');
+
+const script = {
+  config: {
+    target: 'my_awesome_stream',
+    kinesis: {
+      region: 'us-east-1'
+    }
+  },
+  scenarios: [{
+    name: 'Push data to stream',
+    engine: 'kinesis',
+    flow: [
+      {
+        putRecord: {
+          data: 'A very boring payload'
+        }
+      }
+    ]
+  }]
+};
+
+test('Engine interface', function (t) {
+  const events = new EventEmitter();
+  const engine = new KinesisEngine(script, events, {});
+  const scenario = engine.createScenario(script.scenarios[0], events);
+  t.assert(engine, 'Can construct an engine');
+  t.assert(typeof scenario === 'function', 'Can create a scenario');
+  t.end();
+});
